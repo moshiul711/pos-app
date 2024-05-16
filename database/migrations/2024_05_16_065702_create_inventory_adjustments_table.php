0331fb3fd4d5675f78888f1805c85f485d9efffd
moshiul <moshiulislam42@gmail.com>
2024-05-16 13:46:14 +0600

    Product management model controller db completed

---
+++ database/migrations/2024_05_16_065702_create_inventory_adjustments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_adjustments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->enum('adjustment_type',['add','remove']);
+            $table->integer('quantity');
+            $table->text('reason');
+            $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_adjustments');
+    }
+
+};
